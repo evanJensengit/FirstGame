34b76ccfb434bbdc2289ea5d84354a37b0fd5ee2
Evan Jensen <ekylejensen@gmail.com>
2026-05-25 10:19:45 -0700

    whatever we did here fixed the player being controlled by the mouse

--- Player.gd
+++ Player.gd
@@ -5,24 +5,24 @@ signal item_caught(score_modifier: int)
 
 @export var speed: float = 15.0
 var target_x: float = 270.0 # Center of screen default
-var screen_width: float = 740.0
+var screen_width: float = 540.0
 
-#func _ready() -> void:
-	# Center the player near the bottom of the screen
-	#position = Vector2(270, 850)
-	# Connect Godot's built-in collision signal to our own function
-	#area_entered.connect(_on_area_entered)
+func _ready() -> void:
+	#Center the player near the bottom of the screen
+	position = Vector2(270, 850)
+	 #Connect Godot's built-in collision signal to our own function
+	if not area_entered.is_connected(_on_area_entered):
+		area_entered.connect(_on_area_entered)
 
-#func _input(event: InputEvent) -> void:
-	## Handles both mouse clicks/drags and mobile touch events seamlessly
-	#if event is InputEventMouseButton or event is InputEventMouseMotion:
-		#target_x = event.position.x
+func _input(event: InputEvent) -> void:
+	# Handles both mouse clicks/drags and mobile touch events seamlessly
+	if event is InputEventMouseButton or event is InputEventMouseMotion:
+		target_x = event.position.x
 #
 func _physics_process(delta: float) -> void:
 	# Smoothly interpolate (lerp) toward target input for juicy movement
-	global_position = Vector2(270.0, 850.0)
-	#position.x = min(max(40, lerp(position.x, target_x, speed * delta)), screen_width - 40)
-	
+	position.x = lerp(global_position.x, target_x, speed * delta)
+	position.x = clamp(global_position.x, 50.0, screen_width - 50.0)	
 func _on_area_entered(area: Area2D) -> void:
 	# Type assertion / Guard clause
 	if area is FallingItem:
